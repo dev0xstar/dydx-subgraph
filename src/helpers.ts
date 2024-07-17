191025949188585665f44c1b15165414dd3f2470
dev0xstar <angrybird4837@gmail.com>
2024-07-17 19:43:16 -0400

    first commit

---
+++ src/helpers.ts
@@ -0,0 +1,98 @@
+import { Address, BigInt } from '@graphprotocol/graph-ts'
+import { User, Proposal, ProposalVote, RewardsClaimed } from '../generated/schema';
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+
+type ProposalVoteId = string
+
+type RewardsClaimedId = string
+
+export function getUser(address: Address): User {
+  let user = User.load(address.toHexString())
+  if (!user) {
+    user = new User(address.toHexString())
+    user.totalTokens = BigInt.fromI32(0)
+    user.totalStakedTokens = BigInt.fromI32(0)
+    user.votingPower = BigInt.fromI32(0)
+    user.proposingPower = BigInt.fromI32(0)
+    user.tokenVotingPower = BigInt.fromI32(0)
+    user.tokenProposingPower = BigInt.fromI32(0)
+    user.stakedTokenVotingPower = BigInt.fromI32(0)
+    user.stakedTokenProposingPower = BigInt.fromI32(0)
+  }
+
+  return user;
+}
+
+export function getProposal(proposalId: BigInt): Proposal {
+  let proposal = Proposal.load(proposalId.toString())
+  if (!proposal) {
+    proposal = new Proposal(proposalId.toString())
+  }
+
+  return proposal;
+}
+
+export function getProposalVote(proposalId: BigInt, userAddress: Address): ProposalVote {
+  let proposalVoteId: ProposalVoteId = getProposalVoteId(proposalId, userAddress)
+  let proposalVote = ProposalVote.load(proposalVoteId)
+  if (!proposalVote) {
+    proposalVote = new ProposalVote(proposalVoteId)
+  }
+
+  return proposalVote;
+}
+
+function getProposalVoteId(proposalId: BigInt, userAddress: Address): ProposalVoteId {
+  return proposalId.toString() + "-" + userAddress.toHexString()
+}
+
+export function getRewardsClaimed(
+  userAddress: Address,
+  incentivesModule: String,
+  claimTimestamp: BigInt,
+): RewardsClaimed {
+  let userRewardId: RewardsClaimedId = getRewardsClaimedId(
+    userAddress,
+    incentivesModule,
+    claimTimestamp,
+  )
+  let userReward = RewardsClaimed.load(userRewardId)
+  if (!userReward) {
+    userReward = new RewardsClaimed(userRewardId)
+  }
+
+  return userReward;
+}
+
+function getRewardsClaimedId(
+  userAddress: Address,
+  incentivesModule: String,
+  claimTimestamp: BigInt,
+): RewardsClaimedId {
+  return userAddress.toHexString() + "-" + incentivesModule + "-" + claimTimestamp.toString()
+}
+
+export function changeUserTokenBalance(address: Address, amount: BigInt, add: boolean): void {
+  let user: User = getUser(address)
+
+  if (add) {
+    user.totalTokens = user.totalTokens.plus(amount)
+  } else {
+    user.totalTokens = user.totalTokens.minus(amount)
+  }
+
+  user.save()
+}
+
+export function changeUserStakedTokenBalance(address: Address, amount: BigInt, add: boolean): void {
+  let user: User = getUser(address)
+
+  if (add) {
+    user.totalStakedTokens = user.totalStakedTokens.plus(amount)
+  } else {
+    user.totalStakedTokens = user.totalStakedTokens.minus(amount)
+  }
+
+  user.save()
+}
---
+++ src/token.ts
@@ -0,0 +1,28 @@
+import { Address } from "@graphprotocol/graph-ts"
+import {
+  Transfer,
+  DelegatedPowerChanged,
+} from "../generated/DydxToken/DydxToken"
+import { ADDRESS_ZERO, changeUserTokenBalance } from "./helpers";
+import { handleDelegation, DYDXTokenType } from "./delegate";
+
+export function handleTokenTransfer(event: Transfer): void {
+  let from: Address = event.params.from;
+  let to: Address = event.params.to;
+  if (from == to) {
+    // ignore self-transfers
+    return
+  }
+
+  let amount = event.params.value;
+  if (from.toHexString() != ADDRESS_ZERO) {
+    // don't subtract from zero address on mints
+    changeUserTokenBalance(from, amount, false)
+  }
+
+  changeUserTokenBalance(to, amount, true)
+}
+
+export function handleTokenDelegation(event: DelegatedPowerChanged): void {
+  handleDelegation(event.params.user, event.params.amount, event.params.delegationType, DYDXTokenType.Token)
+}
